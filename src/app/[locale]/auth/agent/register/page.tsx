1c60be8147cdc9d96ef8c110eefc9ff19d5ebadf
reham_q <rehamqahwajii@gmail.com>
2025-02-06 05:09:24 +0300

    Fix Agent Register

--- src/app/[locale]/auth/agent/register/page.tsx
+++ src/app/[locale]/auth/agent/register/page.tsx
@@ -1,43 +1,4 @@
-import { AuthCard } from "@/sections";
-import { useTranslations } from "next-intl";
-import { Col, Row, Tabs } from "antd";
-import { HolidayHomesAgentForm } from "@/sections/auth/register/agent/HolidayHomesAgentForm";
-import { RealEstateAgentForm } from "@/sections/auth/register/agent/RealEstateAgentForm";
-import { AuthSideImage } from "@/components";
-
-const items = [
-  {
-    key: "holiday-home",
-    label: "holidayHomesAgent",
-    children: <HolidayHomesAgentForm />,
-  },
-  {
-    key: "real-estate",
-    label: "realEstateAgent",
-    children: <RealEstateAgentForm />,
-  },
-];
+import { AgentRegisterForm } from "@/sections";
 export default function RegisterAgentPage() {
-  const translate = useTranslations("RegisterPage.Agent");
-
-  return (
-    <Row style={{ margin: "24px 5%" }}>
-      {/* Image Column */}
-      <Col xs={0} xl={12}>
-        <AuthSideImage />
-      </Col>
-      {/* Auth Card Column */}
-      <Col xs={24} xl={12}>
-        <AuthCard action={translate("action")}>
-          <Tabs
-            defaultActiveKey="1"
-            items={items.map((item) => ({
-              ...item,
-              label: translate(item.label),
-            }))}
-          ></Tabs>
-        </AuthCard>
-      </Col>
-    </Row>
-  );
+  return <AgentRegisterForm />;
 }
